38e6176642cf9d8bdc3af478c6abf41ef871fb16
daktak <daktak@domo.ddns.net>
2018-05-19 13:29:36 +1000

    initial commit
    Only spathi eluder currently in.
    TODO 
    randomize ships and captains
    config when randomization

---
+++ src/c/main.c
@@ -0,0 +1,185 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+static TextLayer *s_time_layer;
+static TextLayer *s_cap_layer;
+static GBitmap *meri_image;
+static RotBitmapLayer *rot;
+//spathi
+static char eluder_cap[17][10]={"Thwil","Pwappy","Phwiff","Wiffy","Plibnik","Snurfel","Kwimp","Pkunky","Jinkeze","Thintho","Rupatup","Nargle","Phlendo","Snelopy","Bwinkin","Whuff","Fwiffo"};
+//androsynth
+static char guardian_cap[16][10]={"BOOJI-1","DORN-3","BIM-XT","JOR-15","976-KILL","KORB-7B","XR4-TI","CRC-16","BHS-79","DOS-1.0","ME-262","AK-47","1040-EZ","NECRO-99","HAL-2001","SR-71"};
+//ariloulaleelay
+static char skiff_cap[16][10]={"Fefaloo","Bezabu","Tiptushi","Marypup","Tinkafo","Patooti","Tifiwilo","Loleelu","Louifoui","Pinywiny","Oowbabe","Dingdup","Wewalia","Yipyapi","Ropilup","Wolwali"};
+//chenjesu
+static char broodhome_cap[16][10]={"Kzzakk","Tzrrow","Zzmzmm","Vziziz","Hmmhmm","Bzrak","Krrtzz","Zzzzz","Zxzakz","Brrzap","Tzaprak","Pzkrakz","Fzzzz","Vrroww","Zznaz","Zzzhmm"};
+//chmmr
+static char avatar_cap[16][10]={"Mnzgk","Chzrmn","Bzztrm","Zrnzrk","Tzzqrn","Kzzrn","Vzrzn","Qrntz","Rmnzk","Szmrnz","Zbzzn","Frnkzk","Prmtzz","Tzrtzn","Kztztz","Mrnkzt"};
+//druuge
+static char mauler_cap[16][10]={"Tuuga","Siinur","Kaapo","Juugl","Paato","Feezo","Maad","Moola","Kooli","Faazur","Zooto","Biitur","Duulard","Piini","Soopi","Peeru"};
+//human
+static char cruiser_cap[16][10]={"Decker","Trent","Adama","Spiff","Graeme","Kirk","Pike","Halleck","Tuf","Pirx","Wu","VanRijn","Ender","Buck","Solo","Belt"};
+//ilwrate
+static char avenger_cap[16][10]={"Gorgon","Taragon","Kalgon","Borgo","Dirga","Slygor","Rogash","Argarak","Kayzar","Baylor","Zoggak","Targa","Vogar","Lurgo","Regorjo","Manglor"};
+//kohrah
+static char marauder_cap[16][10]={"Death 11","Death 17","Death 37","Death 23","Death 7","Death 13","Death 19","Death 29","Death 31","Death 41","Death 43","Death 3","Death 5","Death 47","Death 53","Death 83"};
+//melnorme
+static char trader_cap[16][10]={"Reddish","Orangy","Aqua","Crimson","Magenta","Cheruse","Beige","Fuchsia","Umber","Cerise","Mauve","Grayish","Yellow","Black","Bluish","Purple"};
+//mmrnhrm
+static char xform_cap[16][10]={"Qir-nha","Jhe-qir","Qua-rhna","Mn-quah","Nrna-mha","Um-hrh","Hm-nhuh","Rrma-hrn","Jra-nr","Ur-mfrs","Qua-qir","Mrm-na","Jhe-mhr","Hmr-hun","Nhuh-na","Hrnm-hm"};
+//mycon
+static char podship_cap[16][10]={"Blort","Chupp","Floos","Glish","Glob","Glush","Plork","Shlish","Shlupp","Slingy","Sploozo","Spork","Uffo","Yush","Zaloop","Znuff"};
+//orz
+static char nemesis_cap[16][10] = {"*Wet*","*Happy*","*Frumple*","*Camper*","*Loner*","*Dancer*","*Singer*","*Heavy*","*NewBoy*","*FatFun*","*Pepper*","*Hungry*","*Deep*","*Smell*","*Juice*","*Squirt*"};
+//pkunk
+static char fury_cap[16][10] = {"Awwky","Tweety","WudStok","Poppy","Brakky","Hooter","Buzzard","Polly","Ernie","Yompin","Fuzzy","Raven","Crow","Jay","Screech","Twitter"};
+//shofixti
+static char scout_cap[18][10] = {"Hiyata","Wasabe","Kudzu","Ichiban","Bonsai!","Genjiro","Ginzu","Busu","Gaijin","Daikon","Sushi","Naninani","Chimchim","Tora-3","Tofu","Kimba","Tanaka","Katana"};
+//slylandro
+static char probe_cap[1][10] = {"2418-B"};
+//supox
+static char blade_cap[16][10] = {"Trifid","Crinoid","FlyTrap","Thistle","Ivy","Sprout","Twig","Root","Branch","Thorn","Bark","Bud","Nut","Stem","Bramble","Thicket"};
+//syreen
+static char penetrator_cap[16][10] = {"Teela","Dejah","Penny","Alia","Be'lit","Ripley","Yarr","Ardala","Sparta","Munro","Danning","Brawne","Maya","Aelita","Alura","Dale"};
+//thraddash
+static char torch_cap[16][10] = {"Dthunk","Bardat","Znonk","Mnump","Bronk","Smup","Grulk","Hornk","Knarg","Drulg","Dgako","Znork","Kwamp","Fkank","Pdump","Whumps"};
+//umgah
+static char drone_cap[16][10] = {"Julg'ka","Gibj'o","Baguk'i","O'guk'e","Gwap'he","Chez'ef","Znork'i","Bob","Kwik'ow","Ei'Ei'o","Brewz'k","Pruk'u","O'bargy","Kterbi'a","Chup'he","I'buba"};
+//urquan
+static char dreadnaugh_cap[16][10] = {"Lord 999","Lord 342","Lord 88","Lord 156","Lord 43","Lord 412","Lord 666","Lord 18","Lord 237","Lord 89","Lord 3","Lord 476","Lord 103","Lord 783","Lord 52","Lord 21"};
+//utwig
+static char jugger_cap[16][10] = {"Endo","Vermi","Manny","Uuter","Nergo","Sami","Duna","Frann","Krisk","Lololo","Snoon","Nestor","Lurg","Thory","Jujuby","Erog"};
+//vux
+static char intruder_cap[16][4] = {"ZIK","PUZ","ZUK","VIP","ZIT","YUK","DAK","ZRN","PIF","FIZ","FUP","ZUP","NRF","ZOG","ORZ","ZEK"};
+//yehat
+static char terminator_cap[16][10] = {"Heep-eep","Feep-eep","Reep-eep","Yeep-eep","Beep-eep","Eeep-eep","Meep-eep","Teep-eep","Jeep-eep","Leep-eep","Peep-eep","Weep-eep","Veep-eep","Geep-eep","Zeep-eep","Neep-eep"};
+//zoqfotpik
+static char stinger_cap[16][8] = {"NikNak","FipPat","DipPak","FatPot","ZikFat","PukYor","TopNik","PorKoo","TikTak","RinTin","FitFap","TotToe","ZipZak","TikTok","MikMok","SikSok"};
+
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  rot_bitmap_layer_set_angle(rot, tick_time->tm_sec * TRIG_MAX_ANGLE / 60);
+  if(tick_time->tm_sec == 0){ // Every minute
+      update_time();
+  }
+}
+
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+  gbitmap_destroy(meri_image);
+  rot_bitmap_layer_destroy(rot);
+  text_layer_destroy(s_cap_layer);
+}
+
+static void main_window_load(Window *window) {
+  // Get information about the Window
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  // Time
+  s_time_layer = text_layer_create(
+      GRect(0, PBL_IF_ROUND_ELSE(8, 2), bounds.size.w, 50)); //58.52
+  // Improve the layout to be more like a watchface
+  text_layer_set_background_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_LECO_28_LIGHT_NUMBERS));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  
+  //Captain Name
+  s_cap_layer = text_layer_create(
+      GRect(0, bounds.size.h - 32, bounds.size.w, 50));
+  text_layer_set_background_color(s_cap_layer, GColorBlack);
+  text_layer_set_text_color(s_cap_layer, GColorWhite);
+  static char s_buffer[8];
+  strcpy(s_buffer, eluder_cap[rand() % 17]);
+  text_layer_set_text(s_cap_layer, s_buffer);
+  text_layer_set_font(s_cap_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  text_layer_set_text_alignment(s_cap_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_cap_layer));
+  
+  //SHIP
+  meri_image = gbitmap_create_with_resource(RESOURCE_ID_ELUDER2);
+  rot = rot_bitmap_layer_create(meri_image);
+  GRect rbounds = layer_get_bounds((Layer*)rot);
+  const GPoint center = grect_center_point(&bounds);
+  GRect image_frame = (GRect) { .origin = center, .size = bounds.size };
+  image_frame.origin.x -= rbounds.size.w/2; //rotlayer does something odd with positioning, and this is the only way I could correct it
+  image_frame.origin.y -= rbounds.size.h/2; //-17
+  layer_set_frame((Layer*)rot,image_frame);
+  rot_bitmap_set_compositing_mode(rot, GCompOpSet);
+  layer_add_child(window_layer, (Layer*)rot);
+}
+
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+  window_set_background_color(s_main_window, GColorBlack);
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+  // Register with TickTimerService
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  // Make sure the time is displayed from the start
+  update_time();
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
+/*
+void handle_second_tick(struct tm *t, TimeUnits units_changed){
+ 
+       
+        if(t->tm_sec == 0){ // Every minute
+               
+                rot_bitmap_layer_set_angle(minute_layer, t->tm_min * TRIG_MAX_ANGLE / 60);
+               
+               
+                if(t->tm_min%5 == 0){
+                        current_min_hand++;
+                        if(current_min_hand == 12) current_min_hand = 0;
+                        bitmap_layer_set_bitmap(thirty_min_layer, small_hands[current_min_hand]);
+                       
+                        if(t->tm_min%10 == 0){ // Every ten minutes
+                                rot_bitmap_layer_set_angle(hour_layer, t->tm_hour%12 * TRIG_MAX_ANGLE / 12 + t->tm_min * TRIG_MAX_ANGLE / (24*30));
+                       
+                            //Every hour
+                                if(t->tm_min == 0){
+                                        current_hr_hand++;
+                                        if(current_hr_hand == 12) current_hr_hand = 0;
+                                        bitmap_layer_set_bitmap(twelve_hour_layer, small_hands[current_hr_hand]);
+                                }
+                }      
+                }
+        }
+}
+*/
