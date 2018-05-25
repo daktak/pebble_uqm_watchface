920a8f321298832c324ceb11677f26cb652ea6d6
daktak <daktak@domo.ddns.net>
2018-05-25 21:36:47 +1000

    Fix turret rotate setting

--- src/c/main.c
+++ src/c/main.c
@@ -505,12 +505,25 @@ static void set_ticker() {
     tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   };
 }
+
+static void reset_timer(int old_rotate) {
+    //reset up timer service
+    if ((old_rotate==1)&& ((settings.ship_rotate == 1)||(settings.turret_rotate == 1))) {
+      tick_timer_service_unsubscribe();
+      set_ticker();
+    } else if ((old_rotate!=1)&& ((settings.ship_rotate == 1)||(settings.turret_rotate == 1))) {
+      tick_timer_service_unsubscribe();
+      set_ticker();
+    }
+}
+
 //inbox
 static void prv_inbox_received_handler(DictionaryIterator *iter, void *context) { 
   Tuple *ship_select_t = dict_find(iter, MESSAGE_KEY_ShipSelection);
   Tuple *ship_change_t = dict_find(iter, MESSAGE_KEY_ShipChange);
   Tuple *ship_rotate_t = dict_find(iter, MESSAGE_KEY_ShipRotate);
   Tuple *cap_change_t = dict_find(iter, MESSAGE_KEY_CapChange);
+  Tuple *turret_rotate_t = dict_find(iter, MESSAGE_KEY_TurretRotate);
   if (ship_select_t) {
     int old_ship = settings.ship_select;
     settings.ship_select = atoi(ship_select_t->value->cstring);
@@ -518,6 +531,17 @@ static void prv_inbox_received_handler(DictionaryIterator *iter, void *context) 
     if (old_ship != settings.ship_select) {
       set_ship();
     }
+  }
+  if (turret_rotate_t) {
+    int old_rotate = settings.turret_rotate;
+    settings.turret_rotate = atoi(turret_rotate_t->value->cstring);
+    //log_int(settings.turret_rotate);
+    if ((old_rotate != settings.turret_rotate)&&(random_race_int == ORZ)) {
+      time_t temp = time(NULL);
+      struct tm *tick_time = localtime(&temp); 
+      rotate_turret(tick_time, settings.turret_rotate);
+    }
+    reset_timer(old_rotate);
   }
   if (ship_rotate_t) {
     int old_rotate  = settings.ship_rotate;
@@ -531,14 +555,7 @@ static void prv_inbox_received_handler(DictionaryIterator *iter, void *context) 
       rotate(tick_time, settings.ship_rotate);
     }
     
-    //reset up timer service
-    if ((old_rotate==1)&& ((settings.ship_rotate == 1)||(settings.turret_rotate == 1))) {
-      tick_timer_service_unsubscribe();
-      set_ticker();
-    } else if ((old_rotate!=1)&& ((settings.ship_rotate == 1)||(settings.turret_rotate == 1))) {
-      tick_timer_service_unsubscribe();
-      set_ticker();
-    }
+    reset_timer(old_rotate);
   }
   if (ship_change_t) {
     settings.ship_change = atoi(ship_change_t->value->cstring);
