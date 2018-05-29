4d5e892b95ad68845b0e20b40d8f556efb630d14
daktak <daktak@domo.ddns.net>
2018-05-29 16:15:19 +1000

    HD2X settings etc

--- src/c/ship.h
+++ src/c/ship.h
@@ -1,5 +1,5 @@
 #pragma once
 #include "src/c/settings.h"
 void ship_unload();
-void create_turret(GRect bounds, Layer *window_layer);
+void create_turret(GRect bounds, Layer *window_layer, bool hd_gfx);
 void set_ship(ClaySettings settings);
